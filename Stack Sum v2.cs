5983ad9ca8644eca07bba6c40e0b39263d24f516
Nikolay37731 <anikolai3773@gmail.com>
2024-10-23 19:21:06 +0300

    New version of the problem
    Tested how updates work by changing the whole file.

---
+++ Stack Sum v2.cs
@@ -0,0 +1,31 @@
+﻿int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Stack<int> stack = new Stack<int>(nums);
+
+string input;
+while ((input = Console.ReadLine()) != "end")
+{
+	input = input.ToLower();
+	string[] tokens = input.Split().ToArray();
+	string command = tokens[0];
+
+	if (command == "add")
+	{
+		int firstNum = int.Parse(tokens[1]);
+		int secondNum = int.Parse(tokens[2]);
+		stack.Push(firstNum);
+		stack.Push(secondNum);
+	}
+	else if (command == "remove")
+	{
+		int numsToRemove = int.Parse(tokens[1]);
+		if (stack.Count >= numsToRemove)
+		{
+			for (int i = 0; i < numsToRemove; i++)
+			{
+				stack.Pop();
+			}
+		}
+	}
+}
+
+Console.WriteLine(stack.Sum());
